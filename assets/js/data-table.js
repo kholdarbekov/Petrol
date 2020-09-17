667e2117fcd8cba00c91e7c5ec553a0262383099
kholdarbekov <international-2014@mail.ru>
2020-09-17 21:07:27 +0500

    datatable event attach problem after pagination fixed

--- assets/js/data-table.js
+++ assets/js/data-table.js
@@ -2,28 +2,6 @@ $(function() {
   'use strict';
 
   $(function() {
-    var dataTableExample = $('#dataTableExample').DataTable({
-      "aLengthMenu": [
-        [10, 30, 50, -1],
-        [10, 30, 50, "All"]
-      ],
-      "iDisplayLength": 10,
-      "language": {
-        search: ""
-      },
-    });
-
-    $('#dataTableExample').each(function() {
-      var datatable = $(this);
-      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
-      var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
-      search_input.attr('placeholder', 'Search');
-      search_input.removeClass('form-control-sm');
-      // LENGTH - Inline-Form control
-      var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
-      length_sel.removeClass('form-control-sm');
-    });
-
     jQuery.expr[':'].regex = function(elem, index, match) {
         var matchParams = match[3].split(','),
         validLabels = /^(data|css):/,
@@ -39,28 +17,51 @@ $(function() {
 
     const csrftoken = Cookies.get('csrftoken');
 
-    $("a:regex(id,^Delete)").on("click",function(e) {
-      e.preventDefault(); // cancel the link itself
-      var result = confirm("Are you sure to delete?");
-      if(!result){
-        return;
-      }
-      var tbRow = this.parentElement.parentElement;
+    var dataTableExample = $('#dataTableExample').DataTable({
+      "aLengthMenu": [
+        [10, 30, 50, -1],
+        [10, 30, 50, "All"]
+      ],
+      "iDisplayLength": 10,
+      "language": {
+        search: ""
+      },
+      "fnDrawCallback": function( oSettings ) {
+        $("a:regex(id,^Delete)").on("click",function(e) {
+          e.preventDefault(); // cancel the link itself
+          var result = confirm("Are you sure to delete?");
+          if(!result){
+            return;
+          }
+          var tbRow = this.parentElement.parentElement;
 
-      $.ajax({
-        url: this.href,
-        type: 'post',
-        data: {
-            pk: this.title
-        },
-        headers: {
-            'X-CSRFToken': csrftoken
-        },
-        //dataType: 'json',
-        success: function (data) {
-            dataTableExample.row(tbRow).remove().draw();
-        }
-      });
+          $.ajax({
+            url: this.href,
+            type: 'post',
+            data: {
+                pk: this.title
+            },
+            headers: {
+                'X-CSRFToken': csrftoken
+            },
+            //dataType: 'json',
+            success: function (data) {
+                dataTableExample.row(tbRow).remove().draw();
+            }
+          });
+        });
+      }
+    });
+
+    $('#dataTableExample').each(function() {
+      var datatable = $(this);
+      // SEARCH - Add the placeholder for Search and Turn this into in-line form control
+      var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
+      search_input.attr('placeholder', 'Search');
+      search_input.removeClass('form-control-sm');
+      // LENGTH - Inline-Form control
+      var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
+      length_sel.removeClass('form-control-sm');
     });
 
   });
