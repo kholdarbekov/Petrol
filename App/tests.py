7c4493ca07ed8c737f7364f507176be58a464cac
kholdarbekov <international-2014@mail.ru>
2020-09-26 20:55:34 +0500

    index page apex line chart height increased from 300 to 450 unittests added

--- App/tests.py
+++ App/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Car, CarModel
 
-# Create your tests here.
+
+class CarTestCase(TestCase):
+    def setUp(self):
+        car_model = CarModel.objects.create(name='new car model')
+        car = Car.objects.create(carNumber='01W000WW', model=car_model)
+        car_model.name = 'test new car model'
+        car_model.save()
+        car.model = car_model
+        car.save()
+
+    def test_car_trades(self):
+        car = Car.objects.get(carNumber='01W000WW')
+        self.assertEqual('test', 'test', 'Message if not equal')
